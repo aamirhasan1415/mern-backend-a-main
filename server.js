6274a8fdb3aa785cc577ccd10dfdee277bdc791e
aamirhasan1415 <aamirhasan2929@gmail.com>
2025-07-10 11:52:46 +0530

    updated

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
 // mongoose
-//   .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
+//   .connect("mongodb://127.0.0.1:27017/merncafe")
 //   .then(() => {
 //     app.listen(8080, () => {
 //       console.log("Server started");
@@ -19,7 +21,7 @@ const dbpass = encodeURIComponent(process.env.DBPASS);
 //   });
 
 mongoose
-  .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.m75vdtm.mongodb.net/merndb`)
+  .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.m75vdtm.mongodb.net/merncafe`)
   .then(() => {
     app.listen(8080, () => {
       console.log("Server started");
